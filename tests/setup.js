badb8491aa88e69e99abef002c6c4421aea9a827
rackoyo <rodrickhoward2@gmail.com>
2025-04-05 13:13:15 -0400

    fix: Add Node.js 16.x support with proper polyfills

--- tests/setup.js
+++ tests/setup.js
@@ -1,11 +1,23 @@
-// Add ReadableStream polyfill for Node.js environments that don't have it
+// Add global polyfills for Node.js environments that don't have them
+const { Readable } = require('stream');
+const { ReadableStream: WebReadableStream } = require('web-streams-polyfill/ponyfill/es2018');
+
+// Add ReadableStream polyfill
 if (typeof ReadableStream === 'undefined') {
-  const { ReadableStream } = require('stream/web');
-  global.ReadableStream = ReadableStream;
+  global.ReadableStream = WebReadableStream;
+}
+
+// Add Blob polyfill if needed
+if (typeof Blob === 'undefined') {
+  const { Blob } = require('buffer');
+  global.Blob = Blob;
 }
 
 // Add fetch polyfill if needed
 if (!global.fetch) {
   const fetch = require('node-fetch');
   global.fetch = fetch;
+  global.Headers = fetch.Headers;
+  global.Request = fetch.Request;
+  global.Response = fetch.Response;
 }
